5f9f8f30b228ce62198cef0ed141692d103e74e6
code-dredd <ray.l.rivera@gmail.com>
2019-12-22 04:58:54 -0400

    lights.models.__init__: Add models package
    Signed-off-by: code-dredd <ray.l.rivera@gmail.com>

---
+++ lights/models/__init__.py
@@ -0,0 +1,16 @@
+'''Database models package.
+
+These models describe the underlying database tables, their structure, and
+other attributes such as data integrity constraints and validation rules.
+
+Validators are used to enforce a data validation layer before database
+constraints take effect on a per-model basis, which can vary depending
+on database server backend. (For more info, see nested packages/modules.)
+'''
+
+from flask_sqlalchemy import SQLAlchemy
+
+
+# model classes depend on the db instance,
+# so it must exist before models are imported
+db = SQLAlchemy()
