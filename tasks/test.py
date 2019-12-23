9eb7e12fe7ea52d7201e170724f4cfa88ee2e65d
code-dredd <ray.l.rivera@gmail.com>
2019-12-23 18:53:09 -0400

    tasks.tests: Add models-only command
    Signed-off-by: code-dredd <ray.l.rivera@gmail.com>

--- tasks/test.py
+++ tasks/test.py
@@ -10,3 +10,9 @@ COMMAND = 'FLASK_ENV=testing coverage run -m pytest -vv --disable-pytest-warning
 def all(c):
     '''Run all available unit tests.'''
     c.run(f'{COMMAND} tests/')
+
+
+@task
+def models(c):
+    '''Run the set of unit tests, but only for data models.'''
+    c.run(f'{COMMAND} tests/models')
