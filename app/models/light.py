f0b48995ac113f23e4224953286f49669d1efea1
code-dredd <ray.l.rivera@gmail.com>
2019-12-30 01:04:30 -0400

    app.models.light: Add `__init__` method
    The method was added to properly handle `bool` types from client
    requests. These are received as `str` types in the `request.form` dict
    and cannot simply be converted using `bool(value)`. For example:
    
      >>> bool('False')
      True
    
    will bite you in a bad place. The tests have been updated to account for
    the fact that we're now setting the `name` field in `__init__`, which
    may raise a `ValidationError` before a `DataIntegrityError` gets a
    chance to be raised. (Validators are auto-run on assignment.)
    
    Signed-off-by: code-dredd <ray.l.rivera@gmail.com>

--- app/models/light.py
+++ app/models/light.py
@@ -52,6 +52,34 @@ class Light(db.Model):
         nullable=False
     )
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.name = kwargs.get('name')
+
+        # Input data may've been received as a `bool` type from an internal
+        # call or as a `str` from a client request. The latter case comes
+        # from the `request.form`'s dictionary and is not auto-converted to
+        # `bool`. So we must handle this case by checking for `truthy` and
+        # `falsey` values we're reasonably willing to accept.
+        #
+        # Here we exclude `None` from `falsey` to raise `ValidationError`s
+        # when the data is actually missing; other falseys, such as empty
+        # lists/sets/tuples, we don't really care about here; client has a
+        # documented API spec to follow, so we're kind of being nice here.
+        #
+        # Avoid just doing `bool(value)` as that can bite you. For example,
+        # a request with `{'is_powered_on': 'False'}` in the dictionary
+        # evals to `True`, i.e. `bool('False') == True` is `True` (any non-
+        # empty `str` is truthy), which is likely not what you want.
+        is_powered_on = kwargs.get('is_powered_on')
+        truthy = (True, 'True', 'true', 1)
+        falsey = (False, 'False', 'false', 0, 0.0)
+        if is_powered_on in truthy:
+            self.is_powered_on = True
+        elif is_powered_on in falsey:
+            self.is_powered_on = False
+
     @validates('name')
     def _validate_name(self, column_name: Text, name_value: Text) -> Text:
         '''Validates the `name` field on assignment.
