2b6b484b9a6ad93afd72973c0d0afc6b117f1878
code-dredd <ray.l.rivera@gmail.com>
2019-12-30 05:20:38 -0400

    wsgi: Add the WSGI app launching module
    This gets used by the dev server launched with `flask run` as well as
    the production uWSGI server later.
    
    Signed-off-by: code-dredd <ray.l.rivera@gmail.com>

---
+++ wsgi.py
@@ -0,0 +1,12 @@
+'''WSGI entrypoint module with callable application object.
+
+This module is used by the uWSGI server, as specified in the
+uWSGI configuration file.
+
+See: conf/uwsgi.ini
+'''
+
+from app import create_app
+from app.settings import FLASK_ENV
+
+app = create_app(FLASK_ENV)
